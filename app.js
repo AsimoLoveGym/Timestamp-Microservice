18bf68088b071b028ceb755cba37bc6a1a50b9b0
AsimoLoveGym <xiazhuangz@gmail.com>
2016-11-28 21:15:55 -0600

    [FEAT & CHORE] Build servide-side js app.js, Use html template express-handlebars for client side, also use express-static for static files, delete some uncessary files

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-
 var handlebars = require('express-handlebars').create({ defaultLayout: null });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-  res.type('text/plain');
-  res.send("Joey's Timestamp Microservice!");
-});
+app.set('port', process.env.PORT || 3000);
 
-app.get('/about', function (req, res) {
-  res.type('text/plain');
-  res.send('Love coding, loce JavaScript!');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
 // custom 404 page
-app.use(function (req, res) {
-  res.type('text/plain');
+app.use(function (req, res, next) {
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 // custom 500 page
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function () {
