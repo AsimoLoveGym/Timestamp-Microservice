473bae723d7ad58685d0001f289e34f0fdc660cc
AsimoLoveGym <xiazhuangz@gmail.com>
2016-11-29 01:44:37 -0600

    [FEAT] Finish the API programming part, also slightly changed the main page content.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var dateFormat = require('dateformat');
 
 var handlebars = require('express-handlebars').create({ defaultLayout: null });
 app.engine('handlebars', handlebars.engine);
@@ -11,6 +12,36 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get('/*', function (req, res) {
+  var queryObject = req.path;
+  var queryString = queryObject.slice(1);
+  var unixNum = 0;
+  var trimedNaturalDate = '';
+  var outPutJson = {};
+  var trimedQueryString = queryString.replace(/%20/g, ' ');
+
+  // Input case 1: unix format, milliseconds num
+  var numCheckPattern = /^[0-9]*$/;
+  if (numCheckPattern.test(queryString)) {
+    unixNum = Number(queryString);
+    var date = new Date(unixNum);
+    trimedNaturalDate = dateFormat(date, 'longDate');
+    outPutJson = {
+      "unix": unixNum,
+      "natural": trimedNaturalDate
+    }
+  } else if (Date.parse(trimedQueryString)) {
+    // Input case 2: natural format, special format should match.
+    unixNum = Date.parse(trimedQueryString);
+    outPutJson = {
+      "unix": unixNum,
+      "natural": trimedQueryString
+    }
+  }
+
+  res.json(outPutJson);
 });
 
 // custom 404 page
